8e70f66c9105d1fe918b2141b403209071fb6c41
TAREK ALJAWI <itachi-t@hotmail.com>
2021-11-18 21:00:17 +0300

    done

--- learn_darts.dart
+++ learn_darts.dart
@@ -1,8 +1,18 @@
-import 'dart:async';
 
-class Learn {
-  void main(){
-    print("aa");
-    
+void seperate(String s){
+  String name = '';
+String domain = '';
+int i=0;
+  for(i;i<s.length;i++){
+    while (!s[i].contains('@')){
+      name+=s[i]; 
+    }
+    domain+=s[i];
   }
+   print("the  user name ${name}, the domain ${domain}");
+}
+
+void main(){
+seperate("s@d.com"); 
+print("lllll");
 }
